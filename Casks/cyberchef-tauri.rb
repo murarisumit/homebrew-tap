2e2a66373d98d95c90e95cd856bf9dab5a06b3f2
murarisumit <murari.sumit@outlook.com>
2026-04-18 21:57:58 +0530

    Update CyberChef Tauri cask SHA256 checksum to match the latest release

--- Casks/cyberchef-tauri.rb
+++ Casks/cyberchef-tauri.rb
@@ -1,6 +1,6 @@
 cask "cyberchef-tauri" do
   version "0.1.4-cyberchef.10.23.0"
-  sha256 "df36caa8a3da5d3f1398c04f8d77c5ab90982d24f9cb0c9ad027bea69bc719a1"
+  sha256 "7d2b501c99dd6971cdb182015d861cefb17723eec969a5d3fbee5fcc6ce8cee3"
 
   url "https://github.com/murarisumit/cyberchef-tauri/releases/download/v#{version}/CyberChef-v#{version}-macos.dmg",
       verified: "github.com/murarisumit/cyberchef-tauri/"
